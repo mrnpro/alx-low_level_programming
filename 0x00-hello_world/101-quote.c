37937e5beca2509c75f9bd2122127e37c84d4c1c
mrnpro <yegetanaty03@gmail.com>
2021-08-29 08:27:17 -0500

    C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@ int main(void)
 {
 	char c[100] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 	int i = 0;
-	while (c[i] !='\0')
+	while (c[i] != '\0')
 	{
 		putchar(c[i]);
 		i++;     
