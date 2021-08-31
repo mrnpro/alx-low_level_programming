301624a459d2312a520e68a746ac768e07a4f9e7
mrnpro <yegetanaty03@gmail.com>
2021-08-31 04:54:56 -0500

    C

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+*print_sign - sends the return value to caller
+*
+*Return: 1 if the number is greater than zero 0 if it is 0 -1 if it is negative
+*/
+int print_sign(int n)
+{
+        if (n > 0)
+                return (1);
+        else if(n < 0)
+                return (0);
+        else
+                return (-1);
+
+}
