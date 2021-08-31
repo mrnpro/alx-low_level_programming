ea2f23eb037797ca0d5fe586073a736f538a627b
mrnpro <yegetanaty03@gmail.com>
2021-08-31 04:21:03 -0500

    C

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+*
+* _isalpha - tells us if the integer given by the caller is matchs with
+*the alphabet ASCII value
+*
+*Return: returns the decision
+/
+
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		if (c >= 97 && c <= 122)
+			return (1);
+	}
+	else
+		return (0);
+}
