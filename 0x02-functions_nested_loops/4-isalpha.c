ed74c760d839c289aa6336c1e783803fbe0838ed
mrnpro <yegetanaty03@gmail.com>
2021-08-31 04:32:41 -0500

    C

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -9,11 +9,13 @@
 
 int _isalpha(int c)
 {
-	if (c >= 65 && c <= 90)
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 	{
-		if (c >= 97 && c <= 122)
-			return (1);
+		return (1);
 	}
 	else
+	{
 		return (0);
+	}
+	_putchar('\n');
 }
