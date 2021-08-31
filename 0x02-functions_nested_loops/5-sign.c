98bf6396520e306eab4c10efa380ae73278d7cb6
mrnpro <yegetanaty03@gmail.com>
2021-08-31 05:00:07 -0500

    C

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,7 +9,7 @@ int print_sign(int n)
         if (n > 0)
                 return (1);
         else if(n == 0)
-                return (0);
+                return (00);
         else
                 return (-1);
 
