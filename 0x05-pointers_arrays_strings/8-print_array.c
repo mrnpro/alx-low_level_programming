24252899c77a2008f5d7a4d472d60480fd1ca85b
mrnpro <yegetanaty03@gmail.com>
2021-09-06 19:42:06 +0000

    this is alx 0x05

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,14 +1,20 @@
 #include "holberton.h"
 #include <stdio.h>
+/**
+*print_array - prints the arrays passed by the main
+*@a: the array thing pointed
+*@n: the size of the array passed by the main
+*/
 void print_array(int *a, int n)
 {
-puts(a);
-puts(", ");
-puts(a++);
-puts(", ");
-puts(a++);
-puts(", ");
-puts(a++);
-puts(", ");
-puts(a++);
+int i = 0;
+
+        for (; i < n; i++)
+        {
+                printf("%d", *(a + i));
+                if (i != (n - 1))
+                        printf(", ");
+        }
+        printf("\n");
+
 }
