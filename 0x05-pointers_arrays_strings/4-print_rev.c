2dc0bd186c0e9f11682b0c4dde41f82ba129dec5
mrnpro <yegetanaty03@gmail.com>
2021-09-06 15:48:47 -0500

    tell the length

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,15 +1,22 @@
 #include "holberton.h"
-#include "string.h"
+
 /**
-*print_rev - reverse print
-*@s: the normal string
-*/
+ * print_rev - prints a string, in reverse, followed by a new line.
+ * @s: input string.
+ * Return: no return.
+ */
 void print_rev(char *s)
 {
-int ch = strlen(s);
-while(ch != 0)
-{
-_putchar(*s+ch);
-ch--;
-}
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if (s[count] == '\0')
+			break;
+		count++;
+	}
+
+	for (count--; count >= 0; count--)
+		_putchar(s[count]);
+	_putchar('\n');
 }
