fe81c600c1b73f99924ca5603768f5c37d620278
mrnpro <yegetanaty03@gmail.com>
2021-09-06 17:54:10 +0000

    this is project 0x05

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,10 +1,10 @@
 #include "holberton.h"
-#include<cstring.h>
+#include <string.h>
 /**
 *_strlen - this tells us the total letters from s 
 *@s: the sentence assigned by the main
 */
 int _strlen(char *s)
 {
-strlen(*s);
+return strlen(*s);
 }
