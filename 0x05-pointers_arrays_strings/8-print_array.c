8c7f9ae5fd8e91161cdf8ee86cb8035007d586f4
mrnpro <yegetanaty03@gmail.com>
2021-09-06 19:24:04 +0000

    this is project 0x05

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 void print_array(int *a, int n)
 {
-printf("%d", a);
-printf(", %d", a++);
-printf(", %d", a++);
-printf(", ", a++);
-printf("%d", a++);
+printf("%p", a);
+printf(", %p", a++);
+printf(", %p", a++);
+printf(", %p", a++);
+printf("%p", a++);
 }
