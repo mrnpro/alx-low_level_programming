5c89227ec538e2d1b006e55c2f8ab87a82406e8d
mrnpro <yegetanaty03@gmail.com>
2021-09-06 19:46:07 +0000

    this is alx 0x05

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,13 +8,12 @@
 void print_array(int *a, int n)
 {
 int i = 0;
-
-        for (; i < n; i++)
-        {
-                printf("%d", *(a + i));
-                if (i != (n - 1))
-                        printf(", ");
-        }
+for (; i < n; i++)
+{
+printf("%d", *(a + i));
+if (i != (n - 1))
+	 printf(", ");
+}
         printf("\n");
 
 }
