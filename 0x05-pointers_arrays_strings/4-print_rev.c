e60c2a3f80139a98a469d4010a143a848e6ee552
mrnpro <yegetanaty03@gmail.com>
2021-09-06 20:27:05 +0000

    this is alx 0x05

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,12 +1,12 @@
 #include "holberton.h"
-#include "stdio.h"
+#include "string.h"
 void print_rev(char *s)
 {
 int i = 0;
 int ch = strlen(s);
 while(ch!=0)
 {
-_putschar(s+ch);
+_putchar(s+ch);
 ch--;
 }
 }
