05e4732f26c0cae5f9e4dba4ea8de9a143e48ebe
mrnpro <yegetanaty03@gmail.com>
2021-09-15 16:00:20 -0500

    this is new edit

---
+++ 1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _print_rev_recursion - reverses the characters.
+ * @s: input string.
+ * Return: no return.
+ */
+void _print_rev_recursion(char *s)
+{
+int i = strlen(s);
+if (i != 0)
+{
+_putchar(s[i - 1]);
+_print_rev_recursion(s - 1);
+}
+
+}
